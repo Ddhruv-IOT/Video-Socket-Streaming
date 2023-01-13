3e8d8d78f7c266c277954162f67daa6aade7a950
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-01-13 19:47:45 +0530

    swapped Sender and Reciever files

--- sender.py
+++ sender.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May  3 15:10:26 2022
+Created on Tue May  3 13:42:51 2022
 
 @author: ACER
 """
+# Sender
 
 import socket
 import cv2
@@ -15,42 +16,32 @@ def get_ip_address():
   s.connect(("8.8.8.8", 80))
   return s.getsockname()[0]
 
-
 # Socket creation
-cln_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host_ip = input(
-    "Kindly input server Ip address \nEx: 192.168.99.1 \nif same as server hit 'y' and enter \nYour Input: ")
+host_ip = get_ip_address()
+print(f"Host IP: {host_ip}")
 
-
-if host_ip == "y":
-    host_ip = get_ip_address()
-
-
-print(f"Reciever IP {host_ip}")
 port = 9999
 socket_address = (host_ip, port)
 
-cln_sock.connect(socket_address)
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-    while len(data) < payload_size:
-        packet = cln_sock.recv(4*1024)
-        if not packet:
-            break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q", packed_msg_size)[0]
+ser_sock.bind(socket_address)
+ser_sock.listen(5)
 
-    while len(data) < msg_size:
-        data += cln_sock.recv(4*1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv2.imshow("Recieved Video", frame)
-    key = cv2.waitKey(1) & 0xff
-    if key == ord('q'):
-        break
-cln_sock.close()
+print(f"Listening at {ser_sock}")
+
+while 1:
+    client, addr = ser_sock.accept()
+
+    print(f"Connected to {addr}")
+    if client:
+        vid = cv2.VideoCapture(1)
+        while(vid.isOpened()):
+            img, frm = vid.read()
+            a = pickle.dumps(frm)
+            message = struct.pack("Q", len(a)) + a
+            client.sendall(message)
+            cv2.imshow('Sent Video', frm)
+            key = cv2.waitKey(1) & 0xff
+            if key == ord('q'):
+                client.close()
